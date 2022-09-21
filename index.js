5527deca718327f80cc35582c2313ac68b7b5b70
Diya2309-coder <diyapatel82008@gmail.com>
2022-09-21 21:39:57 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const http=require("http");
+const fs=require("fs");
+
+let homeContent = ""
+let projectContent="";
+let registrationContent="";
+let styleContent="";
+let scriptContent="";
+
+fs.readFile("home.html",(err,home)=>{
+    if(err){
+        throw err;
+    }
+   homeContent=home;
+});
+
+fs.readFile("project.html",(err,project)=>{
+    if(err){
+        throw err;
+    }
+   projectContent=project;
+});
+
+
+fs.readFile("registration.html",(err,registration)=>{
+    if(err){
+        throw err;
+    }
+   registrationContent=registration;
+});
+
+
+fs.readFile("style.css",(err,style)=>{
+    if(err){
+        throw err;
+    }
+  styleContent=style;
+});
+
+
+
+fs.readFile("script.js",(err,script)=>{
+    if(err){
+        throw err;
+    }
+   scriptContent=script;
+});
+
+http.createServer((request,response)=>{
+    let url=request.url;
+    response.writeHeader(200,{"Content-Type":"text/html"});
+    switch(url){
+        case "/home":
+            response.write(homeContent);
+            response.end();
+            break;
+        case "/project":
+            response.write(projectContent);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationContent);
+            response.end();
+            break;
+        case "/style.css":
+            response.writeHeader(200,{"Content-Type":"text/css"});
+            response.write(styleContent);
+            response.end();
+            break;
+        case "/script.js":
+            response.write(scriptContent);
+            response.end();
+            break;
+        default:
+            response.write(homeContent);
+            response.end();
+            break;
+    }
+    
+})
+.listen(5000);
