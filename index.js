681bce184428f4b76392ae6614fbc227bf511592
Diya2309-coder <diyapatel82008@gmail.com>
2022-09-21 22:06:00 +0530

    modify

--- index.js
+++ index.js
@@ -50,10 +50,10 @@ http.createServer((request,response)=>{
     let url=request.url;
     response.writeHeader(200,{"Content-Type":"text/html"});
     switch(url){
-        case "/home":
-            response.write(homeContent);
-            response.end();
-            break;
+        // case "/home":
+        //     response.write(homeContent);
+        //     response.end();
+        //     break;
         case "/project":
             response.write(projectContent);
             response.end();
